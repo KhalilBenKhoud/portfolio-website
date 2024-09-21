509cdb88d6bd896b3a171799f4174c376e65e200
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-09-21 13:16:10 +0100

    fixed index

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import { EarthCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Experience from './Experience';
+import Works from './Works';
+import Contact from './Contact';
+import Education from './Education';
+import Footer from "./Footer"
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Experience,
+  Works,
+  Contact,
+  EarthCanvas, 
+  ComputersCanvas, 
+  StarsCanvas,
+  Education,
+  Footer
+}
