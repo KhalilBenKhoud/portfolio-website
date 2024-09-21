edd589a02e65b4f5d310513c58dcf740a601a7b1
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-09-21 20:00:18 +0100

    fixed scroll

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh className="pointer-events-none">
+    <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
@@ -61,6 +61,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      onScroll={(e) => e.stopPropagation()}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
