3949c3da9041cf8c8170a655fcb256bcd814e487
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-09-21 19:40:56 +0100

    fixed form

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
@@ -8,7 +9,7 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh>
+    <mesh onPointerMove={(e) => e.stopPropagation()}>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
