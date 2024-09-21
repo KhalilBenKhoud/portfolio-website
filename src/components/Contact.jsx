c6e5e848de774739173e815979212ff66f5a9c0f
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-09-21 13:27:11 +0100

    fixed form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,7 +40,10 @@ const Contact = () => {
           name="contact"  netlify
           className='mt-12 flex flex-col gap-8'
           onSubmit={handleSubmit}
+          method="POST"
         >
+          <input type="hidden" name="form-name" value="contact" />
+
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
             <input
